7600d7c71ef97d626b3428628e6416f13b105385
Luiz Fernando de Almeida Morais <luizfalmeidamorais@gmail.com>
2023-02-25 19:23:15 -0300

    Adicionando o TypeScript e incrementando a forma de desenvolver

--- aula-01/src/index.ts
+++ aula-01/src/index.ts
@@ -1,2 +1,11 @@
+import express, { Request, Response, NextFunction } from 'express'
 
-console.log('Hello World!')
+const app = express()
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'bar' })
+})
+
+app.listen(3000, () => {
+    console.log('Aplicação executando na porta 3000')
+})
